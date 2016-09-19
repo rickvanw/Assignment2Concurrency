0e5ff45306a047b486ee704549892139b63bd4e4
rickvanw <rickvanweersel@hotmail.com>
2016-09-19 16:50:23 +0200

    1

--- src/com/company/Bedrijf.java
+++ src/com/company/Bedrijf.java
@@ -30,9 +30,32 @@ import java.util.concurrent.Semaphore;
 public class Bedrijf {
     private static final int AANTAL_SOFTWAREONTWIKKELAARS = 6;
     private static final int AANTAL_GEBRUIKERS = 10;
+    private Gebruiker[] gebruiker;
+    private Softwareontwikkelaar[] softwareontwikkelaar;
     private ProductOwner productOwner;
     private Semaphore uitnodiging;
 
+    public Bedrijf() {
+
+
+        gebruiker = new Gebruiker[AANTAL_GEBRUIKERS];
+        for(int i=0; i < AANTAL_GEBRUIKERS; i++){
+
+            gebruiker[i] = new Gebruiker("g"+i,i);
+            gebruiker[i].start();
+        }
+
+        softwareontwikkelaar = new Softwareontwikkelaar[AANTAL_SOFTWAREONTWIKKELAARS];
+        for(int i=0; i < AANTAL_SOFTWAREONTWIKKELAARS; i++){
+
+            softwareontwikkelaar[i] = new Softwareontwikkelaar("s"+i,i);
+            softwareontwikkelaar[i].start();
+        }
+
+        productOwner = new ProductOwner();
+        productOwner.start();
+
+    }
 
     private void SoftwareOntwikkelaarOverleg(){
         try{
@@ -48,13 +71,28 @@ public class Bedrijf {
         } catch(InterruptedException e){
 
         }
+
+
     }
 
     public class Softwareontwikkelaar extends Thread{
 
+        private int myId;
+        public Softwareontwikkelaar(String name, int id){
+            super(name);
+            myId = id;
+        }
+
         public void run() {
             while (true) {
                 try {
+
+                    // meldt regelmatig dat beshcikbaar is voor overleg
+
+                    // als projectleider in overleg is, verder met werk
+                    // als projectleider niet in overleg is, wachten voor uitnodiging voor een overleg
+                    // OF tot geconstateerd dat niet bij gesprek hoort, dan verder met werk
+
 
                 } catch (InterruptedException e) {}
             }
@@ -74,9 +112,23 @@ public class Bedrijf {
 
     public class Gebruiker extends Thread{
 
+        private int myId;
+        public Gebruiker(String name, int id){
+            super(name);
+            myId = id;
+        }
+
         public void run() {
             while (true) {
                 try {
+                    // probleem?, melden bij bedrijf
+
+                    // wachten tot uitgenodigd voor overleg
+
+                    // reizen naar bedrijf en melden aangekomen om te overleggen
+
+                    // wachten tot product owner zegt dat gesprek begint
+
 
                 } catch (InterruptedException e) {}
             }
